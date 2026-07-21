f1a710f38bbc9df2479a311fa29a59e48fdeec57
BenClawBot (AlphaOM3G1) <benclawbot@gmail.com>
2026-07-21 15:26:24 +0200

    Add continuity package

---
+++ src/continuity/__init__.py
@@ -0,0 +1,5 @@
+"""Continuity, provenance, and Hermes integration foundations."""
+
+from .models import EventCreate, EventRecord, ScopeRef, TrustAuthority
+
+__all__ = ["EventCreate", "EventRecord", "ScopeRef", "TrustAuthority"]
