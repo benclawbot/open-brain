997295d55e7929dc576b39d45911a583cba50ce0
BenClawBot (AlphaOM3G1) <benclawbot@gmail.com>
2026-07-22 06:47:55 +0200

    Add Codex adapter package exports

---
+++ src/openbrain_codex_adapter/__init__.py
@@ -0,0 +1,6 @@
+"""Codex session bridge for Open Brain."""
+
+from .adapter import CodexSessionAdapter
+from .models import CodexSessionContext
+
+__all__ = ["CodexSessionAdapter", "CodexSessionContext"]
